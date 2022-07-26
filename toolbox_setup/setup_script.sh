b522dc4749fb387bce78e27160295350d188d93e
Rugute <rrugute@gmail.com>
2022-07-26 09:41:43 +0300

    added setup script for autodployment

---
+++ toolbox_setup/setup_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install ansible
+sudo apt install-y openjfx
+sudo mkdir /opt/kehmisApplicationToolbox/
+sudo mkdir /opt/kehmisApplicationToolbox/Downloads
+sudo chmod 777 -R /opt/kehmisApplicationToolbox/
+sudo /usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java --module-path /usr/share/openjfx/lib --add-modules ALL-MODULE-PATH -jar applicationtoolkit-1.0-SNAPSHOT.jar
+
