efc83d0c9f6a4f4a4095af0b6a9ed30edea1c0c6
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2023-10-06 11:24:02 +0300

    Backup and Rollback for O3

--- src/main/resources-unfiltered/openmrs-backup-tools/db_dump.sh
+++ src/main/resources-unfiltered/openmrs-backup-tools/db_dump.sh
@@ -52,25 +52,37 @@ fi
 rollback=/opt/kehmisApplicationToolbox/rollback/
 rollbackDir=/opt/kehmisApplicationToolbox/rollback/db
 modules=/opt/kehmisApplicationToolbox/rollback/modules
+frontend=/opt/kehmisApplicationToolbox/rollback/frontend
 webapps=/opt/kehmisApplicationToolbox/rollback/webapps
+
 #Create rollback
 if [[ ! -e $rollback ]]; then
     mkdir $rollback
 elif [[ ! -d $rollback ]]; then
     echo "$rollback already exists but is not a directory" 1>&2
 fi
+
 #Create db
 if [[ ! -e $rollbackDir ]]; then
     mkdir $rollbackDir
 elif [[ ! -d $rollbackDir ]]; then
     echo "$rollbackDir already exists but is not a directory" 1>&2
 fi
+
 #Create Modules
 if [[ ! -e $modules ]]; then
     mkdir $modules
 elif [[ ! -d $modules ]]; then
     echo "$modules already exists but is not a directory" 1>&2
 fi
+
+#Create Frontend
+if [[ ! -e $frontend ]]; then
+    mkdir $frontend
+elif [[ ! -d $frontend ]]; then
+    echo "$frontend already exists but is not a directory" 1>&2
+fi
+
 #Create Webapps
 if [[ ! -e $webapps ]]; then
     mkdir $webapps
@@ -86,6 +98,7 @@ if [ -d "$rollbackDir" ] &&  [ -w "$rollbackDir" ]; then
 	echo "Copying the backup file to the rollback directory"
 	sudo cp $dumpfile $rollbackDir
 	sudo cp /var/lib/OpenMRS/modules/*.omod $modules
+	sudo cp -rR /var/lib/OpenMRS/frontend/* $frontend
 	sudo cp /var/lib/tomcat9/webapps/openmrs.war $webapps
 	echo "Successfully copied the backup file to the rollback directory"
 fi
